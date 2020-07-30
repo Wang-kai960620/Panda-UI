f182c411c03eb0b1041b5503e442e9c3aafc45ba
wangkai <wk.dong.up@gmail.com>
2020-07-30 19:52:04 +0800

    样式reset

--- dist/app.5cec07dd.js
+++ dist/app.5cec07dd.js
@@ -13324,7 +13324,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53750" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54257" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
