3e45d149638e304676f3ee4753b011a7da4a47c7
wangkai <wk.dong.up@gmail.com>
2020-08-02 17:23:03 +0800

    完成了input输入框，修改了文件app.js

---
+++ src/Input.vue
@@ -0,0 +1,83 @@
+<template>
+    <label class="wrapper" :class="{'error':error}">
+        <input type="text" :disabled="disable" :value="value" :readonly="readonly" :error="error"
+               @change="$emit('change',$event.target.value)"
+               @input="$emit('input',$event.target.value)"
+               @focus="$emit('focus',$event.target.value)"
+               @blur="$emit('blur',$event.target.value)"
+        >
+        <template v-if="error">
+            <Icon name="error" class="icon"></Icon>
+            <span>{{error}}</span>
+        </template>
+    </label>
+</template>
+
+<script lang="ts">
+  import Vue from "vue";
+  import {Component, Prop} from "vue-property-decorator";
+  import Icon from "./Icon.vue";
+
+  @Component({
+    components: {Icon}
+  })
+  export default class Input extends Vue {
+    @Prop({type: Boolean, default: false}) disable: boolean;
+    @Prop({type: Boolean, default: false}) readonly: boolean;
+    @Prop({type: String}) error: string;
+    @Prop(String) value: string;
+    name: "Input";
+  };
+</script>
+
+<style lang="scss" scoped>
+    @import "./assets/help.scss";
+
+    .wrapper {
+        display: inline-block;
+        font-size: 14px;
+
+        > :not(:last-child) {
+            margin-right: 5px;
+        }
+
+        > input {
+            height: 32px;
+            border-radius: 4px;
+            padding: 0 8px;
+            border: 1px solid $border-color;
+            outline: none;
+
+            &:hover {
+                border: 1px solid #40a9ff;
+                box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.1);
+            }
+
+            &:focus {
+                border: 1px solid #40a9ff;
+            }
+
+            &:active {
+                border: 1px solid #40a9ff;
+            }
+        }
+
+        > [disabled] {
+            cursor: not-allowed;
+        }
+
+        > [readonly] {
+            cursor: not-allowed;
+        }
+
+        &.error {
+            > input {
+                border: 1px solid red;
+            }
+
+            > .icon {
+                fill: red;
+            }
+        }
+    }
+</style>
--- src/app.js
+++ src/app.js
@@ -1,22 +1,21 @@
 import Vue from "vue";
-// @ts-ignore
 import Button from "./Button";
-// @ts-ignore
 import Icon from "./Icon";
-// @ts-ignore
 import ButtonGroup from "./Button-Group";
+import Input from './Input'
 
 
 Vue.component("g-button", Button);
 Vue.component("g-icon", Icon);
 Vue.component("g-button-group", ButtonGroup);
+Vue.component('g-input', Input)
 new Vue({
 
-  data: {
-    loading1: false,
-    loading2: false,
-    loading3: false
-  },
-  el: ("#app")
-});
+    data: {
+        loading1: false,
+        loading2: false,
+        loading3: false,
+        message: '请在这里输入！！！'
+    },
+}).$mount('#app')
 
