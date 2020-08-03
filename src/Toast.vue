7267ec06945233465735b4e97dff9464f8a02df6
wangkai <wk.dong.up@gmail.com>
2020-08-03 21:57:55 +0800

    add Toast.vue component

---
+++ src/Toast.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="toast">
+    <slot></slot>
+</div>
+</template>
+
+<script lang="ts">
+  import Vue from "vue";
+  import {Component} from "vue-property-decorator";
+
+  @Component
+  export default class Toast extends Vue {
+    name: "Toast";
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
