428b3d09d3c823fad9d36843f61d3dfc93f193e5
wangkai <wk.dong.up@gmail.com>
2020-08-04 13:35:05 +0800

    toast up down middle animation success

--- src/Toast.vue
+++ src/Toast.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="toast" ref="wrapper" :class="seat">
-        <div class="min">
+    <div class="wrapper" :class="seat">
+        <div class="toast" ref="wrapper" :class="seat">
             <slot></slot>
+            <div class="line" ref="line"></div>
+            <span class="close" v-if="closeButton" @click="onclickClose">{{closeButton[`test`]}}</span>
         </div>
-        <div class="line" ref="line"></div>
-        <span class="close" v-if="closeButton" @click="onclickClose">{{closeButton[`test`]}}</span>
     </div>
 </template>
 
@@ -50,6 +50,7 @@
 
     close() {
       this.$el.remove();
+      this.$emit("close");
       this.$destroy();
     }
 
@@ -65,49 +66,91 @@
 <style lang="scss" scoped>
     @import "assets/help.scss";
 
-    .toast {
+    @keyframes slide-up {
+        0% {
+            opacity: 0;
+            transform: translateY(100%);
+        }
+        100% {
+            opacity: 1;
+            transform: translateY(0%);
+        }
+    }
+
+    @keyframes slide-down {
+        0% {
+            opacity: 0;
+            transform: translateY(-100%);
+        }
+        100% {
+            opacity: 1;
+            transform: translateY(0%);
+        }
+    }
+
+    @keyframes fade-in {
+        0% {
+            opacity: 0;
+        }
+        100% {
+            opacity: 1;
+        }
+    }
+
+    .wrapper {
         position: fixed;
-        min-height: 40px;
         left: 50%;
-        font-size: $font-size;
-        display: flex;
-        align-items: center;
-        background: $bg;
-        border-radius: 5px;
-        box-shadow: 0 0 3px rgba(0, 0, 0, 0.2);
-        padding: 0 16px;
-
-        > .min {
-            display: flex;
-            flex-wrap: wrap;
-            max-width: 300px;
-        }
-
-        > .close {
-            padding-left: 16px;
-            flex-shrink: 0;
-        }
-
-        > .line {
-            border-left: 1px solid #ccc;
-            margin-left: 16px;
-        }
+        transform: translateX(-50%);
 
         &.position-top {
             top: 0;
-            transform: translateX(-50%);
+
+            > .toast {
+                animation: slide-down 300ms;
+            }
         }
 
         &.position-middle {
             top: 50%;
             transform: translate(-50%, -50%);
+
+            > .toast {
+                animation: fade-in 300ms
+            }
         }
 
         &.position-bottom {
             bottom: 0;
-            transform: translateX(-50%);
+
+            > .toast {
+                animation: slide-up 300ms
+            }
+        }
+
+        .toast {
+            min-height: 40px;
+            left: 50%;
+            font-size: $font-size;
+            display: flex;
+            align-items: center;
+            background: $bg;
+            border-radius: 5px;
+            box-shadow: 0 0 3px rgba(0, 0, 0, 0.2);
+            padding: 0 16px;
+
+
+            > .close {
+                padding-left: 16px;
+                flex-shrink: 0;
+            }
+
+            > .line {
+                border-left: 1px solid #ccc;
+                margin-left: 16px;
+            }
+
+
         }
     }
-
 
 </style>
--- src/plugin.js
+++ src/plugin.js
@@ -1,15 +1,31 @@
 import Toast from './Toast'
 
+function creatToast({Vue, message, propsData, onClose}) {
+    const constructor = Vue.extend(Toast)
+    const toast = new constructor({propsData})
+    toast.$slots.default = [message]
+    toast.$mount()
+    toast.$on('close', onClose)
+    document.body.appendChild(toast.$el)
+    return toast
+}
+
+let currentToast
 export default {
     install(Vue, options) {
         Vue.prototype.$toast = function (message, props) {
-            const constructor = Vue.extend(Toast)
-            const toast = new constructor({
-                propsData: props
+            if (currentToast) {
+                currentToast.close()
+            }
+            currentToast = creatToast({
+                Vue,
+                message,
+                propsData: props,
+                onClose: () => {
+                    currentToast = null
+                }
             })
-            toast.$slots.default = [message]
-            toast.$mount()
-            document.body.appendChild(toast.$el)
+
         }
     }
 }
