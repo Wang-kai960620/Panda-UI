e627a40c7c354758dfddd23a818942be550cebd0
wangkai <wk.dong.up@gmail.com>
2020-08-06 19:12:22 +0800

    更行package.json

---
+++ index.js
@@ -0,0 +1,41 @@
+import Button from "./src/Button";
+import Icon from "./src/Icon";
+import ButtonGroup from "./src/Button-Group";
+import Input from './src/Input'
+import Row from './src/Row'
+import Column from "./src/Column";
+import Layout from "./src/Layout";
+import Header from "./src/Header";
+import Sider from "./src/Sider";
+import Footer from "./src/Footer";
+import Content from "./src/Content";
+import Tab from "./src/Tab";
+import TabHead from "./src/Tab-head";
+import TabBody from "./src/Tab-body";
+import TabItem from "./src/Tab-item";
+import TabPane from "./src/Tab-pane";
+import Popover from "./src/Popover";
+import Collapse from "./src/Collapse";
+import CollapseItem from "./src/Collapse-item";
+
+export {
+    Button,
+    Input,
+    Icon,
+    ButtonGroup,
+    Row,
+    Collapse,
+    Column,
+    Layout,
+    Header,
+    Sider,
+    Footer,
+    CollapseItem,
+    Content,
+    Tab,
+    TabBody,
+    TabHead,
+    TabItem,
+    TabPane,
+    Popover
+}
